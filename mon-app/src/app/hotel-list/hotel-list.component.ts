54de8d8495bd0c4e15172dedc1b95de7c6bfe791
mamefaye <mamefaye2017gueye@gmail.com>
2022-12-16 12:38:36 +0000

    Get, Set avec Typescript et création dun filtre de liste

--- mon-app/src/app/hotel-list/hotel-list.component.ts
+++ mon-app/src/app/hotel-list/hotel-list.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IHotel } from './hotel';
 
 @Component({
   selector: 'app-hotel-list',
   templateUrl: './hotel-list.component.html',
-  styleUrls: ['../app.component.css'],
+  styleUrls: ['./hotel-list.component.css']
 })
-export class HotelListComponent {
+export class HotelListComponent implements OnInit {
+
   public title = 'Liste des Hotels';
 
-  public hotels: any[] = [
+  public hotels: IHotel[] = [
     {
       hotelId: 1,
       hotelName: 'Buena Sweet',
@@ -41,10 +43,36 @@ export class HotelListComponent {
 
   public showBadge: boolean = false;
 
-  public hotelFilter = 'Mot';
+  private _hotelFilter = 'Mot';
+
+  public filteredHotels: IHotel[] = [];
+
+  ngOnInit(): void {
+    this.filteredHotels = this.hotels;
+    this.hotelFilter = 'mot';
+  }
 
   public toggleIsNewBadge() {
     this.showBadge = !this.showBadge;
+  }
+
+  public get hotelFilter(): string{
+    return this._hotelFilter;
+  }
+
+  public set hotelFilter(filter: string){
+    this._hotelFilter = filter;
+
+    this.filteredHotels = this.hotelFilter ? this.filterHotels(this.hotelFilter) : this.hotels;
+  }
+
+  private filterHotels(criteria: string): IHotel[] {
+    criteria = criteria.toLocaleLowerCase();
+
+    const res = this.hotels.filter(
+      (hotel: IHotel) => hotel.hotelName.toLocaleLowerCase().indexOf(criteria) !== -1
+    );
+    return res;
   }
 
 }
